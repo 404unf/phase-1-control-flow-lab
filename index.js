abda5ab6a0e34e1de79931fe242c8a57799f926e
Anna <34191811+404unf@users.noreply.github.com>
2023-05-19 23:13:35 -0700

    Conditionals

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let cost;
+  if (feet <= 400) {
+    cost = 'This one is on me!'
+  }
+  else if (feet > 2000 && feet <= 2500){
+    cost = 'I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500) {
+    cost = 'No can do.'
+  }
+  else {
+    cost = 'That will be twenty bucks.'
+  }
+  return cost
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == 'NYC'? 'Ok, sounds good.': 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+  
+    default:
+      return 'Bye.'
+      break;
+  }
 }
